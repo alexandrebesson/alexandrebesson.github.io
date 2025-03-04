fa73641f47f110d21b17f30026f26fe47b6648b3
alexandrebesson <151082772+alexandrebesson@users.noreply.github.com>
2025-03-04 01:22:48 +0100

    ajout initial des fichiers du site web

---
+++ mon_fichier_js.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("bouton");
+let img = document.getElementById("image");
+
+button.addEventListener("click",
+    function () {
+        img.setAttribute("src", "images/doigt2.jpg")
+    }
+);
+        
