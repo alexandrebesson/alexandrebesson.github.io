af74a5c90bed81f03eed804282eb7888c3365835
alexandrebesson <151082772+alexandrebesson@users.noreply.github.com>
2025-03-04 01:13:25 +0100

    Delete mon_fichier_js.js

--- mon_fichier_js.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById("bouton");
-let img = document.getElementById("image");
-
-button.addEventListener("click",
-    function () {
-        img.setAttribute("src", "images/doigt2.jpg")
-    }
-);
-        
